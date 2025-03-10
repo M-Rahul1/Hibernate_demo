058dd08d176c865dcee049c74fa30f41d68681a9
M-Rahul1 <rahul2001ramesh@gmail.com>
2025-03-10 11:00:35 +0530

    first commit

---
+++ hibernate/src/main/java/demo/hibernate/App.java
@@ -0,0 +1,13 @@
+package demo.hibernate;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Alien telusko = new Alien();
+        telusko.setAid(101);
+        telusko.setAname("Navin");
+        telusko.setColor("Green");
+    }
+}
